360bce2e66df87e94d8a6d06a9c6cdb9e337e881
Lisa Peña <37710253+Adventurenow23@users.noreply.github.com>
2020-07-22 17:34:04 -0500

    Updates

--- src/component/Map.js
+++ src/component/Map.js
@@ -3,91 +3,83 @@
 
 //Map is constructed using Google Maps Javascript API and generates markers in connection with App.js//
 
+import React, { Component } from 'react'
+import '../App.css'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 
-import React, { Component } from 'react';
-import '../App.css';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+const MyMapComponent = withScriptjs(
+	withGoogleMap((props) => (
+		<GoogleMap
+			defaultZoom={7}
+			zoom={props.zoom}
+			defaultCenter={{ lat: 41.821975, lng: -88.088488 }}
+			center={{
+				lat: parseFloat(props.center.lat),
+				lng: parseFloat(props.center.lng)
+			}}
+			options={{
+				scrollwheel: false,
+				mapTypeControl: false,
+				fullscreenControl: false,
+				streetViewControl: false,
+				zoomControl: false
+			}}
+		>
+			{props.markers &&
+				props.markers.filter((marker) => marker.isVisible).map((marker, idx, arr) => {
+					const venueInfo = props.venues.find((venue) => (venue.id = marker.id))
 
+					return (
+						<Marker
+							key={idx}
+							tabindex="0"
+							position={{ lat: marker.lat, lng: marker.lng }}
+							onClick={() => props.handleMarkerClick(marker)}
+							animation={
+								arr.length === 1 || marker.isOpen ? (
+									window.google.maps.Animation.BOUNCE
+								) : (
+									window.google.maps.Animation.DROP
+								)
+							}
+						>
+							{marker.isOpen &&
+							venueInfo.bestPhoto && (
+								<InfoWindow aria-label=" infowindow">
+									<React.Fragment>
+										<img
+											src={`${venueInfo.bestPhoto.prefix}200x200${venueInfo.bestPhoto.suffix}`}
+											alt={'venueInfo.name'}
+										/>
+										<p className="info-name">{venueInfo.name}</p>
+									</React.Fragment>
+								</InfoWindow>
+							)}
+						</Marker>
+					)
+				})}
+		</GoogleMap>
+	))
+)
 
-const MyMapComponent = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={7} 
-    zoom = {props.zoom}
-    defaultCenter={{ lat: 41.821975, lng: -88.088488 }} 
-    center = {{
-      lat: parseFloat(props.center.lat),
-      lng: parseFloat(props.center.lng)
-    }}  
-    options = {{
-      scrollwheel: false,
-      mapTypeControl: false,
-      fullscreenControl: false,
-      streetViewControl: false,
-      zoomControl: false,
-  }}
-    >
-    
-    {props.markers && props.markers.filter(marker => marker.isVisible).map((marker,idx,arr) => {
-    const venueInfo = props.venues.find(venue => venue.id = marker.id);
-    
-    return(
-
-    <Marker 
-    key = {idx} 
-    tabindex = "0"
-    position={{ lat: marker.lat, lng: marker.lng }}
-    onClick = {() => props.handleMarkerClick(marker)}
-    animation = {arr.length === 1 || marker.isOpen
-    ? window.google.maps.Animation.BOUNCE : window.google.maps.Animation.DROP}
-    >
-
-    {marker.isOpen && venueInfo.bestPhoto && (
-    <InfoWindow aria-label =" infowindow">
-      <React.Fragment>
-
-
-        
-    <img src = {`${venueInfo.bestPhoto.prefix}200x200${venueInfo.bestPhoto.suffix}`} alt = {"venueInfo.name"}/>
-    <p className = "info-name">{venueInfo.name}</p>
-    </React.Fragment>
-    </InfoWindow>
-    )}
-    </Marker>
-    );
-    })}
-
-  </GoogleMap>
-))
-  
 export default class Map extends Component {
-    render () {
-window.gm_authFailure = () => {
-  this.props.handleError(`
+	render() {
+		window.gm_authFailure = () => {
+			this.props.handleError(`
   GoogleMaps API failed to authenticate.
   Please ensure that your API key and/or or client ID are correct`)
+		}
+
+		return (
+			<MyMapComponent
+				role="application"
+				aria-label="map"
+				{...this.props}
+				googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCfpgeBLmhLfKXWZ699Jud0pAgHMeVFUug"
+				loadingElement={<div className="myMap" />}
+				containerElement={<div className="myMap-container" />}
+				mapElement={<div className="myMap" />}
+			/>
+		)
+	}
 }
-
-return (
-  
-<MyMapComponent
-role = "application"
-aria-label = "map"
-  {...this.props}
-  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCB76fbSiinj4O4gu6U2DTI_lydhwBwpG8"
-  loadingElement={<div className='myMap' />}
-  containerElement={<div className='myMap-container' />}
-  mapElement={<div className='myMap'/>}
-/>
-
-    );
-}
-}
-
-
-
-
-
-
-
-  
-  
